e8cddefa18282baba6bc8bfe5d65d4e4442468b3
Ben Tsai <betsai@cisco.com>
2020-06-11 15:11:20 -0400

    Copy favicons to site

--- .eleventy.js
+++ .eleventy.js
@@ -39,6 +39,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy({ favicons: "/" });
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
