c2a2ea4ecc51ef7ee3af44438f54fe787ee9207f
Ben Tsai <betsai@cisco.com>
2020-08-05 22:22:51 -0400

    Turn off inserting line breaks

--- .eleventy.js
+++ .eleventy.js
@@ -1,74 +1,74 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
+const { DateTime } = require("luxon")
+const fs = require("fs")
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const pluginNavigation = require("@11ty/eleventy-navigation")
+const markdownIt = require("markdown-it")
+const markdownItAnchor = require("markdown-it-anchor")
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(pluginSyntaxHighlight)
+  eleventyConfig.addPlugin(pluginNavigation)
 
-  eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true)
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias("post", "layouts/post.njk")
 
-  eleventyConfig.addFilter("readableDate", (dateObj) => {
+  eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toLocaleString(
       DateTime.DATE_HUGE
-    );
-  });
+    )
+  })
 
   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
-  });
+  eleventyConfig.addFilter("htmlDateString", dateObj => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd")
+  })
 
   // Get the first `n` elements of a collection.
   eleventyConfig.addFilter("head", (array, n) => {
     if (n < 0) {
-      return array.slice(n);
+      return array.slice(n)
     }
 
-    return array.slice(0, n);
-  });
+    return array.slice(0, n)
+  })
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
+  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"))
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy({ favicons: "/" });
+  eleventyConfig.addPassthroughCopy("img")
+  eleventyConfig.addPassthroughCopy("css")
+  eleventyConfig.addPassthroughCopy({ favicons: "/" })
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
     html: true,
-    breaks: true,
+    breaks: false,
     linkify: true,
   }).use(markdownItAnchor, {
     permalink: true,
     permalinkClass: "direct-link",
     permalinkSymbol: "#",
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+  })
+  eleventyConfig.setLibrary("md", markdownLibrary)
 
   // Browsersync Overrides
   eleventyConfig.setBrowserSyncConfig({
     callbacks: {
       ready: function (err, browserSync) {
-        const content_404 = fs.readFileSync("_site/404.html");
+        const content_404 = fs.readFileSync("_site/404.html")
 
         browserSync.addMiddleware("*", (req, res) => {
           // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
+          res.write(content_404)
+          res.end()
+        })
       },
     },
     ui: false,
     ghostMode: false,
-  });
+  })
 
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
@@ -94,5 +94,5 @@ module.exports = function (eleventyConfig) {
       data: "_data",
       output: "_site",
     },
-  };
-};
+  }
+}
